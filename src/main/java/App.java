30948305a4e487e3af11ef0504a67710be3e3b1a
Sergey <chmihun@gmail.com>
2024-08-30 16:00:54 +0300

    first commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,10 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        Cat CatBean =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(CatBean.getMessage());
+
     }
 }
